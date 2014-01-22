c6be0ba5e80b9f33d44b2840d427ac30dc81e5a6
Matt Johnson <johnson.matthew.h@gmail.com>
2014-01-22 15:35:07 -0500

    added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyqueue_wrapper',
+      version='1.0',
+      description=' Wrapper for popular queues like Amazon SQS and iron.IO',
+      author='Matthew H. Johnson, PharmD',
+      author_email='johnson.matthew.h@gmail.com',
+      url='https://bitbucket.org/mhjohnson/pyqueue_wrapper/overview',
+      packages=['__init__.py'],
+     )
